40260adf03cef9a55e82171a86748be3c69f0245
Reinier Schoof <rschoof@transip.nl>
2021-07-21 22:44:56 +0200

    switched to generically read status of all sensors

--- cmd/tempread/main.go
+++ cmd/tempread/main.go
@@ -19,6 +19,14 @@ var (
 		Name: "temperature",
 		Help: "Temperature in hundreds of degrees Celsius",
 	}, []string{"uid"})
+	sensorStatus = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "sensor_status",
+		Help: "Every parsable status for each sensor",
+	}, []string{"uid", "name", "type"})
+
+	ignoreStates = map[string]bool{
+		"lastupdated": true,
+	}
 )
 
 func init() {
@@ -94,14 +102,52 @@ func main() {
 		}
 
 		for _, sensor := range sensors {
-			if sensor.Type != "ZLLTemperature" {
-				continue
-			}
-
 			sLog := log.WithFields(log.Fields{
 				"sensor": sensor.Name,
 				"uid":    sensor.UniqueID,
 			})
+
+			sensorLabel := prometheus.Labels{
+				"uid":  sensor.UniqueID,
+				"name": sensor.Name,
+			}
+
+			for name, state := range sensor.State {
+				// should we just ignore this state name
+				if _, ok := ignoreStates[name]; ok {
+					log.WithField("type", name).Debug("ignoring sensor state")
+
+					continue
+				}
+
+				sensorLabel["type"] = name
+				ssLog := sLog.WithFields(log.Fields{
+					"type":    name,
+					"reading": state,
+				})
+
+				// decide how we should interpret the value from this sensor
+				switch s := state.(type) {
+				case float64:
+					sensorStatus.With(sensorLabel).Set(s)
+				case bool:
+					f := 0.0
+					if s {
+						f = 1.0
+					}
+					sensorStatus.With(sensorLabel).Set(f)
+				default:
+					ssLog.Debug("could not register sensor state")
+					continue
+				}
+
+				ssLog.Info("registered sensor state")
+			}
+
+			// backwards compatibility
+			if sensor.Type != "ZLLTemperature" {
+				continue
+			}
 
 			temp := -1.0
 			for name, state := range sensor.State {
@@ -123,6 +169,7 @@ func main() {
 			}).Set(temp)
 
 			sLog.WithField("temp", temp).Info("registered temperature")
+			// end of backwards compatibility
 		}
 	}
 }
